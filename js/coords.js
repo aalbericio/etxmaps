a33c5bfb74c218a6f18ff443518703322697f4e2
Alberto Albericio <alberto.albericio.schneider@gmail.com>
2016-03-17 19:01:30 +0100

    Added geoJSON stuff

--- js/coords.js
+++ js/coords.js
@@ -3,18 +3,36 @@ var lat_max = 41.46320757;
 var lon_min = 2.10684613;
 var lon_max = 2.25857193;
 
+var features = {
+	"features": [
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [2.183169, 41.4056929]
+			}
+		}, 
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [2.193169, 41.4156929]
+			}
+		}
+	]
+}
 
 function generateRandomNumber(min, max) {
     return Math.random() * (max - min) + min;
 }
             
- function generateLat(){
-     return generateRandomNumber(lat_min, lat_max);
- }
+function generateLat(){
+	return generateRandomNumber(lat_min, lat_max);
+}
 
- function generateLon(){
-     return generateRandomNumber(lon_min, lon_max);
- }
+function generateLon(){
+	return generateRandomNumber(lon_min, lon_max);
+}
 
 function generateNCoords(num){
     var coords = [];
